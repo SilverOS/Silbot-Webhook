2b109e60b0f7fb6ea64a6f10fde7deb074ec6a6f
SilverOS <36743499+SilverOS@users.noreply.github.com>
2018-12-01 23:21:46 +0100

    Updated security

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <?php
-echo '<h1 align="center">SilBot v 1.1 </h1>';
-$token   = $_GET['api'];
+echo '<h1 align="center">SilBot X v 1.0 </h1>';
+if (isset($_GET['api'])) $token   = $_GET['api'];
 $api     = $token;
-$idadmin = $_GET['admin'];
-$adminID = $idadmin;
-$userbot = $_GET['userbot'];
-$admin   = array( //lista admin
-    $adminID
-);
+if (isset($_GET['userbot']))$userbot = $_GET['userbot'];
 
 $content = file_get_contents("php://input");
 $update  = json_decode($content, true);
 $config  = array(
     "db" => true, // true per usare un database mysql, false per non usarlo
-    "tipo_db" => "mysql", //"mysql" per un database mysql, "json" per un database attraverso file json
-    "anti_post_bug" => true, //Questa é una misura di sicurezza per evitare che alcune persone creino delle richieste false danneggiando il database mysql cambiando il parametro userbot
+    // Sicurezza
+    "automatic_username_check" => true, //Controlla automaticamente se l'username del bot è corretto,per migliori prestazioni è consigliato disabiltarlo e abilitare bot_username.
+    "bot_username" => array( //Per maggiore sicurezza inserisci gli username dei tuoi bot che sono collegati al webhook,ciò eviterà cloni
+        "allowed_usernames" => array("Silbot","Sil2bot"), //username dei vari bot,SONO CASE-SENSITIVE
+        "active" => false, //Se lo abiliti i cloni non saranno possibili se non presenti nell'array sopra,
+    ),
+    "ip_check" => array( //NON FUNZIONA CON CLOUDFLARE Evita delle richieste indesiderate da esterni
+      "whitelist" => array(), //Inserisci gli ip che potranno fare richieste oltre a quelli di Telegram
+      "enabled" => false, //Consiglio: Disabilita se sei in testing o stai impostando il webhook con @devtoolsforbot
+    ),
     //MYSQL
-    "altervista" => false, //true se usi altervista, false se non lo usi
-    "antiflood" => false, //true per abilitare l'antiflood, false per disabilitarlo. Trovi il file in plugins/antiflood.php. Funziona solo con Mysql
     "ip" => "localhost", // se non usi altervista metti l'indirizzo del database, di norma localhost se è hostato sullo stesso server
     "user" => "root", //se non usi altervista inserisci il nome utente del DB
     "password" => "psw", //se non usi altervista inserisci la password di mysql
     "database" => "db", //se non usi altervista inserisci il nome del database
-    //Json
-    "jsondbname" => "database.json", //Il nome del file che contiene il database json o il path
     //TELEGRAM
     "action" => false, //true per mandare azioni come typing... e false per non mandare nulla
     "parse_mode" => "HTML", //Formattazione presefinita messaggio, HTML, Markdown o none
@@ -34,26 +33,30 @@ $config  = array(
     "funziona_canali" => true //Scegli se far eseguire i messaggi inviati dai canali
 );
 
-if ($config["anti_post_bug"] && $userbot && $config["tipo_db"] == "mysql") {
+if ($config["automatic_username_check"] && isset($userbot)) {
     $info = json_decode(file_get_contents("http://api.telegram.org/$api/getMe"), true);
     if ($info["result"]["username"] != $userbot) {
         exit;
     }
 }
-include("vars.php");
-include("functions.php");
-if ($config['db']) {
-    include("database.php");
-}
-include("comandi.php");
-$plugins = scandir("plugins");
-unset($plugins[0]);
-unset($plugins[1]);
-$disabled = array(
-    "pluginno.php"
-); //Qui và la lista dei plugin disabilitati
-foreach ($plugins as $plug) {
-    if (!in_array($plug, $disabled)) {
-        include("plugins/$plug");
+if ($config["bot_username"]["active"] && isset($userbot)) {
+    if (!in_array($userbot,$config["bot_username"]["allowed_usernames"])) {
+        exit;
     }
+}
+if ($config["ip_check"]["active"] && isset($token)) {
+    if (stripos($_SERVER['REMOTE_ADDR'],'149.154.167')!==false) {
+    } else {
+        if (!in_array($_SERVER['REMOTE_ADDR'],$config["ip_check"]["whitelist"])) {
+            exit;
+        }
+    }
+}
+if (isset($_GET["api"])) {
+    include("vars.php");
+    include("functions.php");
+    if ($config['db']) {
+        include("database.php");
+    }
+    include("comandi.php");
 }
