73df495afc63ddbeb81c16b8a527e2d69dbbc923
SilverOS <36743499+SilverOS@users.noreply.github.com>
2019-06-08 10:29:32 +0200

    Add files via upload

--- commands.php
+++ commands.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($bot)) {
-    if (isset($callback)) {
+    if ($update->type === 'callback_query' && isset($callback)) {
         if ($callback->data == 'ciao' && isset($user)) {
-            $bot->editMessageText($message->chat->id,$message->message_id,'Ciao!');
-            $r = $bot->answerCallbackQuery($callback->id,'Ok');
+            $bot->editMessageText($message->chat->id, $message->message_id, 'Ciao!');
+            $r = $bot->answerCallbackQuery($callback->id, 'Ok');
         }
     } else {
-        if (isset($message->text)) {
+        if (isset($message->text) && $update->type === 'message') {
             if ($message->text == '/start') {
                 $keyboard[] = [['text' => 'ciao', 'callback_data' => 'ciao']];
                 $r = $bot->sendMessage($user->id, 'Ciao', $keyboard);
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
-isset($_GET['token']) ? $token = 'bot'.$_GET['token'] : $token = false;
+isset($_GET['token']) ? $token = 'bot' . $_GET['token'] : $token = false;
 
 require 'config.php';
-new update(file_get_contents('php://input'),$token,$config);
+$bot = new botApi($token,$config);
+$update = new update(file_get_contents('php://input'), $token, $config);
+//$bot->sendMessage(141691961,json_encode($update->update,JSON_PRETTY_PRINT));
+if (isset($update->message)) {
+    $message = $update->message;
+    if (isset($message->photo)) $photo = $message->photo;
+    if (isset($message->audio)) $audio = $message->audio;
+    if (isset($message->voice)) $voice = $message->voice;
+    if (isset($message->animation)) $animation = $message->animation;
+    if (isset($message->document)) $document = $message->document;
+    if (isset($message->video)) $video = $message->video;
+    if (isset($message->video_note)) $video_note = $message->video_note;
+    if (isset($message->contact)) $contact = $message->contact;
+    if (isset($message->location)) $location = $message->location;
+    if (isset($message->venue)) $venue = $message->venue;
+    if (isset($message->poll)) $poll = $message->poll;
+}
+if (isset($update->chat)) $chat = $update->chat;
+if (isset($update->user)) $user = $update->user;
+if (isset($update->callback)) $callback = $update->callback;
+include 'commands.php';
