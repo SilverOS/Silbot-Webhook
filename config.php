cd025d79e0807f29e804c6ed355ef8d57d348929
SilverOS <36743499+SilverOS@users.noreply.github.com>
2019-06-25 11:57:55 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -2,10 +2,14 @@
 
 
 $config = [
+    'token_whitelist' => [ //Avoid receiving fake requests or not allowed cloned bots
+        'tokens' => ['botTOKEN'],
+        'enabled' => false
+    ],
     'database' => [
         'active' => true,
         'ip' => 'localhost',
-        'user' => 'root',
+        'user' => '',
         'password' => '',
         'db_name' => '',
         'universal_table' => 'users', // Where are stored Telegram User Information
@@ -16,6 +20,11 @@ $config = [
     'disable_notification' => false,
     'disable_web_page_preview' => false,
 ];
+if ($config['token_whitelist']['enables']) {
+    if (!in_array($token,$config['token_whitelist']['tokens'])) {
+        exit;
+    }
+}
 if ($config['database']['active']) {
     require 'functions/database.php';
 }
